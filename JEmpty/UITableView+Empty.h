9e9a7aa2c7ea683b0e450539b666c76dfedde63b
Fqah <osmk@qq.com>
2016-03-08 18:46:54 +0800

    first commit

---
+++ JEmpty/UITableView+Empty.h
@@ -0,0 +1,13 @@
+//
+//  UITableView+Empty.h
+//  JEmpty
+//
+//  Created by __无邪_ on 3/8/16.
+//  Copyright © 2016 fqah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableView (Empty)
+
+@end
---
+++ JEmpty/UITableView+Empty.m
@@ -0,0 +1,13 @@
+//
+//  UITableView+Empty.m
+//  JEmpty
+//
+//  Created by __无邪_ on 3/8/16.
+//  Copyright © 2016 fqah. All rights reserved.
+//
+
+#import "UITableView+Empty.h"
+
+@implementation UITableView (Empty)
+
+@end
